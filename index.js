261201054effee55a452a1743b9921992a336e8b
fregnani123 <fabianofregnani@hotmail.com>
2023-07-30 19:59:46 -0300

    estudos

--- index.js
+++ index.js
@@ -17,39 +17,91 @@
 //     console.log(c) 
 //  }
 
-const pessoas = [
-    { nome: "Alice", cidade: "Içara/SC" },
-    { nome: "Bob", cidade: "Criciúma/SC" },
-    { nome: "Carlos", cidade: "São Paulo/SP" },
-    { nome: "Diana", cidade: "Rio de Janeiro/RJ" },
-    { nome: "Eduardo", cidade: "Porto Alegre/RS" },
-    { nome: "Fernanda", cidade: "Curitiba/PR" },
-    { nome: "Gabriel", cidade: "Belo Horizonte/MG" },
-    { nome: "Helena", cidade: "Salvador/BA" },
-    { nome: "Isaac", cidade: "Recife/PE" },
-    { nome: "Julia", cidade: "Fortaleza/CE" }
-  ];
+// const pessoas = [
+//   { nome: "Alice", cidade: "Içara/SC" },
+//   { nome: "Bob", cidade: "Criciúma/SC" },
+//   { nome: "Carlos", cidade: "São Paulo/SP" },
+//   { nome: "Diana", cidade: "Rio de Janeiro/RJ" },
+//   { nome: "Eduardo", cidade: "Porto Alegre/RS" },
+//   { nome: "Fernanda", cidade: "Curitiba/PR" },
+//   { nome: "Gabriel", cidade: "Belo Horizonte/MG" },
+//   { nome: "Helena", cidade: "Salvador/BA" },
+//   { nome: "Isaac", cidade: "Recife/PE" },
+//   { nome: "Julia", cidade: "Fortaleza/CE" }
+// ];
 
-  function buscar() {
-    const nomeBusca = document.querySelector("#buscar").value;
-    const res = document.querySelector('#res');
-    if (!nomeBusca) {
-      res.innerHTML ='Digite um nome para buscar.';
+// pessoas.push({nome:'Pedro',cidade:'Içara/SC'})
+
+// console.log(pessoas)
+
+//   function buscar() {
+//     const nomeBusca = document.querySelector("#buscar").value;
+//     const res = document.querySelector('#res');
+//     if (!nomeBusca) {
+//       res.innerHTML ='Digite um nome para buscar.';
+//     } else {
+//       const resultado = pessoas.filter(pessoa => pessoa.nome.toLowerCase().includes(nomeBusca.toLowerCase()));
+//       if (resultado.length === 0) {
+//         res.innerHTML='Nenhum resultado encontrado.';
+//       } else {
+//         console.log('Resultados encontrados:');
+//         resultado.forEach(pessoa => res.innerHTML = `Nome: ${pessoa.nome}<br> Cidade: ${pessoa.cidade}`);
+//       }
+//     }
+//   }
+
+
+// // //   console.log(pessoas[2].nome); // 
+  
+
+const pessoas = [
+  { nome: "Alice", cidade: "Içara/SC" },
+  { nome: "Bob", cidade: "Criciúma/SC" },
+  { nome: "Carlos", cidade: "São Paulo/SP" },
+  { nome: "Diana", cidade: "Rio de Janeiro/RJ" },
+  { nome: "Eduardo", cidade: "Porto Alegre/RS" },
+  { nome: "Fernanda", cidade: "Curitiba/PR" },
+  { nome: "Gabriel", cidade: "Belo Horizonte/MG" },
+  { nome: "Helena", cidade: "Salvador/BA" },
+  { nome: "Isaac", cidade: "Recife/PE" },
+  { nome: "Julia", cidade: "Fortaleza/CE" }
+];
+
+function adicionarPessoa() {
+  const nome = document.querySelector("#nome").value;
+  const cidade = document.querySelector("#cidade").value;
+
+  if (nome && cidade) {
+    pessoas.push({ nome, cidade });
+    console.log("Nova pessoa adicionada:", { nome, cidade });
+  } else {
+    console.log("Por favor, preencha todos os campos.");
+  }
+}
+
+function buscar() {
+  const nomeBusca = document.querySelector("#buscar").value;
+  const res = document.querySelector('#res');
+
+  if (!nomeBusca) {
+    res.innerHTML = 'Digite um nome para buscar.';
+  } else {
+    const resultado = pessoas.filter(pessoa => pessoa.nome.toLowerCase().includes(nomeBusca.toLowerCase()));
+
+    if (resultado.length === 0) {
+      res.innerHTML = 'Nenhum resultado encontrado.';
     } else {
-      const resultado = pessoas.filter(pessoa => pessoa.nome.toLowerCase().includes(nomeBusca.toLowerCase()));
-      if (resultado.length === 0) {
-        res.innerHTML='Nenhum resultado encontrado.';
-      } else {
-        console.log('Resultados encontrados:');
-        resultado.forEach(pessoa => res.innerHTML = `Nome: ${pessoa.nome}<br> Cidade: ${pessoa.cidade}`);
-      }
+      res.innerHTML = ''; // Clear previous search results
+
+      console.log('Resultados encontrados:');
+      resultado.forEach(pessoa => {
+        const div = document.createElement('div');
+        div.innerHTML = `Nome: ${pessoa.nome}<br> Cidade: ${pessoa.cidade}`;
+        res.appendChild(div);
+      });
     }
   }
-
-
-//   console.log(pessoas[2].nome); // 
-  
- 
+}
 
   
   
